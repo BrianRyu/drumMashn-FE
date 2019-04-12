3dd45ebbaa268713bdfdf2d15ac8388258e336dc
WSINTRA <will.simpson85@gmail.com>
2019-04-12 14:00:03 -0400

    fixed naming conventions

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const divSkinTag = document.querySelector('.my-skin')
-const skinUrl = 'http://localhost:3000/api/v1/drum_machine'
+const skinUrl = 'http://localhost:3000/api/v1/skin'
 fetch(skinUrl)
 .then( res => res.json() )
 .then( skin => {
 divSkinTag.innerHTML += `<img src=${skin[0].skin_url}>` 
 } )
+
