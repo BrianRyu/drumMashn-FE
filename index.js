ad245744e42cc953f11de6373cdfb53c95308a4a
brianryu <s.brianryu@gmail.com>
2019-04-16 11:03:20 -0400

    welcoming font animation

--- index.js
+++ index.js
@@ -382,3 +382,25 @@ document.addEventListener('keypress', function (e) {
     }
 });
 
+// OPENING TEXT ANIMATION JS ******************************************************************************
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.ml15 .word',
+    scale: [14,1],
+    opacity: [0,1],
+    easing: "easeOutCirc",
+//     duration: 800,
+    delay: function(el, i) {
+      return 800 * i;
+    }
+   })
+//    .add({
+//     targets: '.ml15',
+//     opacity: 0,
+//     duration: 1000,
+//     easing: "easeOutExpo",
+//     delay: 1000
+//   });
+
+//   END OF OPENING TEXT ANIMATION // ***********************************************************************
