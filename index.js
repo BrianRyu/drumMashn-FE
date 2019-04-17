ba5087a2b7d23f9e95ffdb703a548934af1ac648
brianryu <s.brianryu@gmail.com>
2019-04-17 14:48:54 -0400

    Create Kit and Add Sound btns fixed to pop up Modal onclick

--- index.js
+++ index.js
@@ -47,31 +47,48 @@ dropDownTag.addEventListener('click', (event) => {
 ///////////////////////////////////////////////
 
 // Get the modal
-const modal = document.getElementById('soundModal');
+const soundModal = document.getElementById('soundModal');
+const kitModal = document.getElementById('kitModal');
 
 // Get the button that opens the modal
-const btn = document.getElementsByClassName("modalButton");
+const soundBtn = document.getElementById("addSoundBtn");
+const kitBtn = document.getElementById("createKitBtn");
 
 // Get the <span> element that closes the modal
-const span = document.getElementsByClassName("close")[0];
+const soundSpan = document.getElementById("soundSpan");
+const kitSpan = document.getElementById("kitSpan");
+
 
 
 // When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
+soundBtn.onclick = function() {
+  soundModal.style.display = "block";
 }
-
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+soundSpan.onclick = function() {
+    soundModal.style.display = "none";
+}
+// When the user clicks anywhere outside of the modal, close it
+document.onclick = function(event) {
+    if (event.target == soundModal) {
+      soundModal.style.display = "none";
+    }
 }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+
+kitBtn.onclick = function() {
+    kitModal.style.display = "block";
 }
+
+kitSpan.onclick = function() {
+    kitModal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == kitModal) {
+      kitModal.style.display = "none";
+    }
+  }
 //////////////////////////////////////////////
 // ENDING LOGIC BEHIND MODAL FOR ADD SOUND ///
 //////////////////////////////////////////////
